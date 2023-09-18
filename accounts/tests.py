7c3af626e341cd0660234e4c1fc35bda280e5479
biba <aytbayb@inbox.ru>
2023-09-18 14:30:26 +0600

    added signup, login and logout functions with test(only signup)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -24,5 +25,13 @@ class CustomUserTests(TestCase):
         self.assertTrue(superuser.is_staff)
         self.assertTrue(superuser.is_superuser)
 
+class SignUpPageTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
     
-    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign up')
+        self.assertNotContains(self.response, 'I should not be here')
