b31f1bad1c88c56a66e641a3986bf04039a46c94
biba <aytbayb@inbox.ru>
2023-09-19 16:45:00 +0600

    added bootstrap5 with crispy_forms

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomePageTests(SimpleTestCase):
     def setUp(self):
@@ -24,3 +24,23 @@ class HomePageTests(SimpleTestCase):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
         
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+    
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+    
+    def test_aboutpage_containts_correct_html(self):
+        self.assertContains(self.response, 'About page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'I should not be here')
+    
+    def test_aboutpage_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
