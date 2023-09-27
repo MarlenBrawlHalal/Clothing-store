0a751c9e95b39c0a6b181b0ab49b51fa9c01b781
biba <aytbayb@inbox.ru>
2023-09-27 14:58:17 +0600

    added search function

--- clothes/urls.py
+++ clothes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ClothesListView, ClothesDetailView
+from .views import ClothesListView, ClothesDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', ClothesListView.as_view(), name='clothes_list'),
     path('<uuid:pk>/', ClothesDetailView.as_view(), name='clothes_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- clothes/views.py
+++ clothes/views.py
@@ -1,12 +1,30 @@
+from typing import Any
+from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Clothes
 
-class ClothesListView(ListView):
+class ClothesListView(LoginRequiredMixin, ListView):
     model = Clothes
     context_object_name = 'clothes_list'
     template_name = 'clothes/clothes_list.html'
+    login_url = 'account_login'
 
-class ClothesDetailView(DetailView):
+class ClothesDetailView(LoginRequiredMixin, DetailView):
     model = Clothes
     context_object_name = 'clothes'
     template_name = 'clothes/clothes_detail.html'
+    login_url = 'account_login'
+
+class SearchResultsListView(ListView):
+    model = Clothes
+    context_object_name = 'clothes_list'
+    template_name = 'clothes/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Clothes.objects.filter(
+            Q(title__icontains=query)
+        )
